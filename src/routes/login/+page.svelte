daaff977160aef019bb4848f74d0ce82a0e3f0ce
ClarenceAng <clarence_ivan_ang@dlsu.edu.ph>
2025-06-09 15:22:14 +0800

    test

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script src="https://accounts.google.com/gsi/client" async></script>
+<div
+  id="g_id_onload"
+  data-client_id="490000336368-3fg0brv9lgvsbe33tmft6mrk402dmc5u.apps.googleusercontent.com"
+  data-context="signin"
+  data-ux_mode="redirect"
+  data-callback="handleSignInWithGoogle"
+  data-nonce=""
+  data-auto_select="true"
+  data-itp_support="true"
+  data-use_fedcm_for_prompt="true"
+ data-login_uri="https://euwhpolzjpfuqncfjczc.supabase.co/auth/v1/callback"
+  data-auto_prompt="false"
+></div>
+
+<div
+  class="g_id_signin"
+  data-type="standard"
+  data-shape="pill"
+  data-theme="outline"
+  data-text="signin_with"
+  data-size="large"
+  data-logo_alignment="left"
+></div>
