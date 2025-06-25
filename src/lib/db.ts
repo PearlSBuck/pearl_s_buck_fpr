b740d1f7c7a8dfffd3c9dbc49911632270f8a70d
ClarenceAng <clarence_ivan_ang@dlsu.edu.ph>
2025-06-25 18:05:15 +0800

    changed small thing in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://euwhpolzjpfuqncfjczc.supabase.co",
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? "",
   import.meta.env.VITE_SUPABASE_KEY ?? "" // Ensure to replace with your actual Supabase project URL and key
 );
