f807bd8a22c5b251b81aeeb9f31e2445806833b9
PyroRSC <109332014+PyroRSC@users.noreply.github.com>
2025-06-25 16:41:53 +0800

    config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUAPABASE_ANON_KEY ?? "",
-  process.env.SUPABASE_KEY ?? "" // Ensure to replace with your actual Supabase project URL and key
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? "",
+  import.meta.env.VITE_SUPABASE_KEY ?? "" // Ensure to replace with your actual Supabase project URL and key
 );
