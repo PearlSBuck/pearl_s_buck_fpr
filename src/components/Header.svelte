aeb5778db9adc2cc1be08a57fa5491c0d805fc80
ClarenceAng <clarence_ivan_ang@dlsu.edu.ph>
2025-06-26 23:14:21 +0800

    hello

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   export let name: string = '';
+  export let search: boolean = false;
+  export let backButton: boolean = false;
 </script>
 
 <div class="fixed top-0 left-0 right-0 z-50 bg-[#F6F8FF]">
@@ -25,7 +27,17 @@
     </svg>
   </div>
 
-  <div class="bg-[#474C58] h-10 flex items-center">
-    <p class="ml-10 text-white text-lg font-semibold">{name}</p>
+  <div class="bg-[#474C58] h-10 flex justify-between items-center">
+    <span class="flex items-center gap-4 ml-4">
+      <p class="ml-10 text-white text-lg font-semibold">{name}</p>
+      {#if backButton}
+      <button on:click={() => history.back()} class="bg-[#1A5A9E] text-white font-semibold py-0.5 px-3 rounded-lg cursor-pointer">Back</button>
+      {/if}
+    </span>
+    {#if search}
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-10">
+    <path d="M21 21L15 15M17 10C17 10.9193 16.8189 11.8295 16.4672 12.6788C16.1154 13.5281 15.5998 14.2997 14.9497 14.9497C14.2997 15.5998 13.5281 16.1154 12.6788 16.4672C11.8295 16.8189 10.9193 17 10 17C9.08075 17 8.1705 16.8189 7.32122 16.4672C6.47194 16.1154 5.70026 15.5998 5.05025 14.9497C4.40024 14.2997 3.88463 13.5281 3.53284 12.6788C3.18106 11.8295 3 10.9193 3 10C3 8.14348 3.7375 6.36301 5.05025 5.05025C6.36301 3.7375 8.14348 3 10 3C11.8565 3 13.637 3.7375 14.9497 5.05025C16.2625 6.36301 17 8.14348 17 10Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    {/if}
   </div>
 </div>
