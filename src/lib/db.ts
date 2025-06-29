5f95a52021a2484828c7c768577f71a73f8ea879
Nate Adiong <nathaniel_irvin_adiong@dlsu.edu.ph>
2025-06-30 01:03:06 +0800

    edited db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 // src/lib/db.ts
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL ?? "",
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? ""
+);
