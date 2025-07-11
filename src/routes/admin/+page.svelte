aff368da76e056c6a8be48d0ae0ccdef948dcca7
ClarenceAng <clarence_ivan_ang@dlsu.edu.ph>
2025-07-12 00:30:25 +0800

    Delete Records

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -5,14 +5,15 @@
 <script lang="ts">
   import Header from '../../components/Header.svelte'; // adjust the path as needed
   import Year from '../../components/Year.svelte'; // adjust the path as needed
+  import Confirm from '../../components/Confirm.svelte'; // adjust the path as needed
 
   let pageName = "rinaldo baldcado";
   let innerText = "Year: 2023";
   let selected: 'progress_report' | 'intro_sheet' = 'progress_report';
 </script>
 <Header name={pageName} search backButton />;
-<div class="mt-100 mx-10">
-  <Year innerText={innerText} activated={true} />
+<div class="mt-75 mx-10">
+  <Confirm show={true}/>
 </div>
 
 
