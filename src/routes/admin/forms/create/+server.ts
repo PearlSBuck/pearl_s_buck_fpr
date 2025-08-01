d16efdba21c8af2b73320517ec80efca80b19d2e
PyroRSC <109332014+PyroRSC@users.noreply.github.com>
2025-08-02 03:01:59 +0800

    bugs

--- src/routes/admin/forms/create/+server.ts
+++ src/routes/admin/forms/create/+server.ts
@@ -2,17 +2,8 @@
  * Backend for processing form data
  */
 import { json } from '@sveltejs/kit';
-import { Pool } from "pg";
 import { createForm, createFormSection, createFormFieldsPerSection } from './manage_forms.server';
-const POSTGRES_URL = process.env.POSTGRES_URL!;
 
-
-const pgDb = new Pool({
-    connectionString: POSTGRES_URL,
-    ssl: { rejectUnauthorized: false }
-});
-
-export default pgDb;
 
 export const POST = async ({ request }) => {
     const { form, formSections, sectionFields } = await request.json();
